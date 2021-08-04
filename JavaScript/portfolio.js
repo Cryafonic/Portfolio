7c9a1d0e90af8e366c15868006d8d309964f9fea
Brendon <Brendonvandernest69@gmail.com>
2021-08-04 23:01:56 +0200

    Add class active

--- JavaScript/portfolio.js
+++ JavaScript/portfolio.js
@@ -1,6 +1,18 @@
 $(document).ready(() => {
+    // toggles active class on nav
     $(".toggle").click(() => {
-        $(".toggle").toggleClass('active');
-        $(".showcase").toggleClass('active');
+        $(".toggle").toggleClass("active");
+        $(".showcase").toggleClass("active");
     });
+
+    let setTime = 2000
+
+    let list = document.querySelectorAll('.icons a img');
+
+    // add classof active on floating icons
+    for (let i = 0; i < list.length; i++) {
+        setTimeout(() => {
+            list[i].classList.add("active");
+        }, setTime = setTime + 1000);
+    }
 })
